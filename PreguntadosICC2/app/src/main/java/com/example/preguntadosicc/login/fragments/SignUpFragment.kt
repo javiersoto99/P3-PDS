68ff2a4e94d1480ee15b91002f05cf164044f090
laifmartinez <ramartinez2@miuandes.cl>
2021-10-13 23:05:43 -0300

    register funcional

--- PreguntadosICC2/app/src/main/java/com/example/preguntadosicc/login/fragments/SignUpFragment.kt
+++ PreguntadosICC2/app/src/main/java/com/example/preguntadosicc/login/fragments/SignUpFragment.kt
@@ -1,16 +1,26 @@
 package com.example.preguntadosicc.login.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.example.preguntadosicc.R
 import com.example.preguntadosicc.login.LoginViewModel
 import com.example.preguntadosicc.login.models.RegisterInfo
+import com.example.preguntadosicc.networking.UsersRemoteRepository
+import com.example.preguntadosicc.networking.getRetrofit
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
 
 class SignUpFragment : Fragment() {
     private val mLoginviewModel: LoginViewModel by activityViewModels()
@@ -34,8 +44,7 @@ class SignUpFragment : Fragment() {
             val password =view.findViewById<EditText>(R.id.passwordEditText).text.toString()
             val user = RegisterInfo(email, username, password)
 
-            mLoginviewModel.registerUser(user)
-            mLoginviewModel.navigator.navigateToLogin()
+            signUp(user)
         }
 
 
@@ -43,5 +52,29 @@ class SignUpFragment : Fragment() {
         return view
     }
 
+    private fun signUp(user: RegisterInfo){
+        val service = getRetrofit(okHttpClient = OkHttpClient()).create(UsersRemoteRepository::class.java)
 
+        service.registerUser(user).enqueue(object : retrofit2.Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(
+                    context,
+                    t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+
+
+                if (response.code() == 201) {
+                    Toast.makeText(context, "El usuario fue creado exitosamente", Toast.LENGTH_SHORT).show()
+
+                    mLoginviewModel.navigator.navigateToLogin()
+                } else {
+                    Toast.makeText(context, "The user already exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+    }
 }
