7e679f302a8eda202eb550c61dd3c835ad88df58
javiersoto99] <jisoto2@miuandes.cl>
2021-11-01 18:31:40 -0300

    historial

---
+++ PreguntadosICC2/app/src/main/java/com/example/preguntadosicc/main/Perfil/HistorialAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.preguntadosicc.main.Perfil
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.preguntadosicc.R
+import com.example.preguntadosicc.main.models.Friend
+
+class HistorialAdapter: RecyclerView.Adapter<HistorialAdapter.HistorialViewHolder>() {
+
+    private var partidas = mutableListOf<String>()
+
+    inner class HistorialViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+        val resultado = itemView.findViewById(R.id.resutado_tv) as TextView
+        val detallesBtn = itemView.findViewById<Button>(R.id.detalles_btn)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistorialViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.historial_view_holder, parent, false)
+        return HistorialViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HistorialViewHolder, position: Int) {
+        //ToDo
+
+    }
+
+    override fun getItemCount(): Int {
+        return partidas.size
+    }
+
+}
+
+
